5ed2942c2c0c67f8d000f90390d3f66eda29baf8
jdustinwilliams <jdustin@andrew.cmu.edu>
2023-01-12 13:21:48 -0500

    Add basic ProvenanceRecord DB seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Based on a helpful gist: https://gist.github.com/arjunvenkat/1115bc41bf395a162084
+require 'csv'
+
+file_path = Rails.root.join('lib', 'seeds', 'collections.csv')
+
+if !File.exist?(file_path)
+    # Give red-colored error message
+    puts "\e[31mERROR: #{file_path} not found.\e[0m"
+    exit
+end
+
+csv_text = File.read(file_path)
+csv = CSV.parse(csv_text, headers: true, encoding: 'UTF-8')
+
+csv.each do |row|
+    p = ProvenanceRecord.new
+    p.accession_number = row[0]
+    puts "Accession #: #{row[0]}"
+    p.collection_name = row['collection_name']
+    p.collection_name_alpha = row['collection_name_alpha']
+    p.start_year = row['start_year']
+    p.end_year = row['end_year']
+    p.date_expression = row['date_expression']
+    p.linear_feet = row['linear_feet']
+    p.containers = row['containers']
+
+    if (row[2] && row[2].include?("X"))
+        p.unlist = true
+    else
+        p.unlist = false
+    end
+
+    p.save
+end
