2c487b14897611dc879e4a8b473722b44b487394
jdustinwilliams <jdustin@andrew.cmu.edu>
2023-01-17 13:50:25 -0500

    Add delete confirmation page

--- app/controllers/provenance_records_controller.rb
+++ app/controllers/provenance_records_controller.rb
@@ -69,6 +69,10 @@ class ProvenanceRecordsController < ApplicationController
     end
   end
 
+  def confirm_destroy
+    @provenance_record = ProvenanceRecord.find(params[:provenance_record_id])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_provenance_record
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :provenance_records
+  resources :provenance_records do
+    get :confirm_destroy
+  end
 end
